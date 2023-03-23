665e7cd696684caae8be0fb3d48d5d081f3f594b
Tatiana Ivanova <tat.mi.iva@gmail.com>
2023-03-23 12:15:42 +0300

    feat: add SectionWithForm component

---
+++ src/components/SectionWithForm.js
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import Form from "./Form";
+
+function SectionWithForm({
+  title,
+  name,
+  onSubmit,
+  isFormValid,
+  isLoading,
+  loadingText,
+  submitBtnText,
+  theme,
+  children,
+}) {
+  return (
+    <section className="content section">
+      <h2 className="section__title">{title}</h2>
+      <Form
+        name={name}
+        onSubmit={onSubmit}
+        isFormValid={isFormValid}
+        isLoading={isLoading}
+        loadingText={loadingText || "Загрузка..."}
+        submitBtnText={submitBtnText}
+        theme={theme || "dark"}>
+        {children}
+      </Form>
+      {name === "singup" && (
+        <p className="section__text">
+          Уже зарегистрированы? <Link to="/sign-in" className="section__link">Войти</Link>
+        </p>
+      )}
+    </section>
+  );
+}
+
+export default SectionWithForm;
